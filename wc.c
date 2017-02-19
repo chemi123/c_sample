72be287727324a231459902faed3cee3f10967ed
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-02-19 15:58:06 +0900

    fix

--- wc.c
+++ wc.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void do_wc(const char *path);
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: %s FILENAME", argv[0]);
     exit(1);
   }
 
-  FILE *file_stream = fopen(argv[1], "r");
-  if (!file_stream) {
-    perror(argv[1]);
-    exit(1);
-  }
-
-  char buf[1024];
-  int cl = 0;
-  while (fgets(buf, sizeof buf, file_stream) != NULL) {
-    cl++;
-  }
-  printf("%d\n", cl);
-
-  fclose(file_stream);
+  do_wc(argv[1]);
 
   exit(0);
+}
+
+static void
+do_wc(const char *path) {
+  FILE *file_stream = fopen(path, "r");
+  if (!file_stream) {
+    perror(path);
+    exit(1);
+  }
+  
+  int c;
+  int cl = 0;
+  while ((c = fgetc(file_stream)) != EOF) {
+    if (c == '\n') cl++;
+  }
+
+  printf("%d\n", cl);
 }
