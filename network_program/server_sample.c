9b2dfc2353762e4126677780df485c5bbfea7e03
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-03-16 01:51:06 +0900

    update server_sample.c

--- network_program/server_sample.c
+++ network_program/server_sample.c
@@ -7,45 +7,48 @@
 #include <unistd.h>
 
 int main() {
-  int server_sock_fd, client_sock_fd;
+  int server_socket_fd, client_socket_fd;
   int server_len, client_len;
   struct sockaddr_in server_address;
   struct sockaddr_in client_address;
 
-  server_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_sock_fd < 0) {
+  // socketの作成
+  server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket_fd < 0) {
     perror("socket: ");
     exit(1);
   }
-  server_address.sin_family = AF_INET;
+
+  server_address.sin_family= AF_INET;
   server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
   server_address.sin_port = 9734;
-
   server_len = sizeof(server_address);
-  if (bind(server_sock_fd, (struct sockaddr *)&server_address, server_len) < 0) {
+  if (bind(server_socket_fd, (struct sockaddr *)&server_address, server_len) < 0) {
     perror("bind: ");
     exit(1);
   }
 
-  if (listen(server_sock_fd, 5) < 0) {
+  if (listen(server_socket_fd, 5)) {
     perror("listen: ");
     exit(1);
   }
-  
-  while(1) {
-    //char ch;
 
+  while (1) {
     printf("server waiting\n");
-    
-    client_sock_fd = accept(server_sock_fd, (struct sockaddr *)&client_address, &client_len);
 
-    //read(client_sock_fd, &ch, 1);
-    //ch++;
-    write(client_sock_fd, "abcde", 5);
-    close(client_sock_fd);
+    client_socket_fd = accept(server_socket_fd, (struct sockaddr *)&server_address, &client_len);
+    if (client_socket_fd < 0) {
+      perror("accept: ");
+      break;
+    }
+
+    if (write(client_socket_fd, "abcde", 5) < 0) {
+      perror("write: ");
+      break;
+    }
+
+    close(client_socket_fd);
   }
 
-  close(server_sock_fd);
-
-  exit(0);
+  close(server_socket_fd);
 }
